420f53d8745b421470f2c7a8580ddf0005de71a5
Boris Rorsvort <boris@rorsvort.com>
2010-12-26 15:46:03 +0100

    add trnaslation for model tables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => [:show, :edit, :update]
- 
+  def index
+    @users = User.all
+  end
   def new
     @user = User.new
   end
