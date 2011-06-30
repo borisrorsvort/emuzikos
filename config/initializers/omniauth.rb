23b64e835f29dc3a9a4f3210385894151b9cb273
Boris Rorsvort <boris@rorsvort.com>
2011-06-30 19:14:37 +0200

    change api facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   require 'openid/store/filesystem'
   
   #provider :facebook, '226327037396547', '8367d544c64b1c3c3b55ff807c74fabb' real account
-  provider :facebook, '226327037396547', '8367d544c64b1c3c3b55ff807c74fabb'
+  if RAILS_ENV == "development"
+    provider :facebook, '226327037396547', '8367d544c64b1c3c3b55ff807c74fabb'
+  elsif RAILS_ENV == "staging"
+    provider :facebook, '242929185733501', 'a66d6e3973cefa07946503f3aef1cca7'
+  else
+    provider :facebook, '226327037396547', '8367d544c64b1c3c3b55ff807c74fabb'
+  end
         
   provider :twitter, '76zeThKPzGSMABaPJRxfA', 'CLPWwv4ZUGe2hVth09JAzkvqi8veVbslb7bH3CavBY'
     
