94b2a0ebc0b9bb8d6021829ac779248a0abb1d10
Boris Rorsvort <boris@rorsvort.com>
2011-07-07 21:44:45 +0200

    new assets

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
       @users = User.profiles_completed.paginate(:page => params[:page], :per_page => AppConfig.site.results_per_page, :order => sort_column + " " + sort_direction)
     end
     if request.xhr?
-      sleep(10) # make request a little bit slower to see loader :-)
+      sleep(2) # make request a little bit slower to see loader :-)
       render :partial => @users
     end
   end
