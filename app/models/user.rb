49695af3d8bee602f8520cdb6f81f7b1636eadaf
Boris Rorsvort <boris.rorsvort@gmail.com>
2011-10-06 11:45:21 +0200

    added meta_where operators but broke active admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,6 +69,8 @@ class User < ActiveRecord::Base
     :s3_headers => {'Expires' => 1.year.from_now.httpdate},
     :default_url => '/images/backgrounds/no-image-:style.gif'
 
+  #scope :profiles_completed, where( :country != nil, :user_type != nil, :genre != nil, :zip != nil )
+  #scope :profiles_completed, where("country IS NOT NULL and user_type IS NOT NULL and genre IS NOT NULL and zip IS NOT NULL ")
   scope :profiles_completed, where( :country ^ nil, :user_type ^ nil, :genre ^ nil, :zip ^ nil)
   scope :currently_signed_in, where( "last_sign_in_at > ?", 1.hours.ago )
   scope :except_current_user, lambda { |user| where("users.id != ?", user.id) }
--- lib/tasks/development.rake
+++ lib/tasks/development.rake
@@ -1,7 +1,8 @@
 namespace :development do
   desc "create first admin"
   task :create_admin =>:environment do
-    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
+    u = AdminUser.new(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
+    u.save
     if u.save! && u.valid?
       puts "Admin succesfully created"
     else
