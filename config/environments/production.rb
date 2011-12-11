c74b7274a0fe8aba4687e5bc120bd26a64c841f8
Boris Rorsvort <boris.rorsvort@gmail.com>
2011-12-11 23:04:41 +0100

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,8 +6,8 @@ Emuzikos::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.action_controller.consider_all_requests_local = true
-  config.action_view.debug_rjs                         = true
+  config.action_controller.consider_all_requests_local = false
+  #config.action_view.debug_rjs                         = true
   config.action_controller.perform_caching             = false
 
   config.active_support.deprecation = :notify
