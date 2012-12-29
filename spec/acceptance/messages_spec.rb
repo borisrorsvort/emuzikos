329cc736a633459905367a989841adc44f324bb7
Boris Rorsvort <boris.rorsvort@trustedfamily.net>
2012-12-29 01:46:46 +0100

    fix message test

--- spec/acceptance/messages_spec.rb
+++ spec/acceptance/messages_spec.rb
@@ -10,8 +10,8 @@ feature "Send a Message", :js => true do
 
   scenario "to visible user" do
     visit(user_path(@recipient))
-    fill_in 'Subject', :with => "HI from Belgium"
-    fill_in 'Message', :with => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
+    fill_in 'message_subject', :with => "HI from Belgium"
+    fill_in 'message_body', :with => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
     click_button('Submit')
     page.should have_content('Success')
     current_path.should == social_share_path(:invite_friends)
