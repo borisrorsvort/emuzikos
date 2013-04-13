8d90747d27c9fc4d1b8092d0d69aff2ace368a41
Boris Rorsvort <boris.rorsvort@trustedfamily.net>
2013-04-13 12:09:14 +0200

    simplify omniauth controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,48 +2,30 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   require 'carmen'
   include Carmen
 
+  before_filter :request_env_data
+
   def facebook
+    provider = 'facebook'
+    auth     = find_provider('facebook')
 
-    omniauth = request.env['omniauth.auth']
-    data = omniauth.extra.raw_info
-
-    auth = Service.find_by_provider_and_uid("facebook", data.id.to_s)
     if auth
-      session[:origin] = "Facebook"
-      flash[:notice] = t(:'services.signed_in_successful_via') + " facebook"
-      sign_in_and_redirect(:user, auth.user)
+      sign_in_with_provider(auth, provider)
     else
-      existinguser = User.where(:email => data.email).first
+      existinguser = User.where(:email => @data.email).first
 
       if existinguser
-        existinguser.services.create(:provider => "facebook", :uid => data.id.to_s, :uname => data.username, :uemail =>  data.email)
-        flash[:notice] = t(:'services.logged_in_via') + " Facebook " + t(:'services.added_to_account') + " " + data.email + " " + t(:'services.signed_in_successful')
-        session[:origin] = "Facebook"
-        sign_in_and_redirect(:user, existinguser)
+        create_facebook_auth(existinguser)
       else
-        data.username = data.username[0, 39] if data.username.length > 39
-        user = User.new( :email =>  data.email,
+        @data.username = @data.username[0, 39] if @data.username.length > 39
+        user = User.new( :email =>  @data.email,
                          :password => Devise.friendly_token[0,20],
                          :user_type => "musician",
-                         :username => data.username.gsub(/ /,'').downcase.parameterize
+                         :username => @data.username.gsub(/ /,'').downcase.parameterize
                         )
 
-        if omniauth.extra.raw_info.location.present?
-          locationName    = data.location.name
-          coordinates     = Geocoder.coordinates(locationName)
-          locationResult  = Geocoder.search(locationName).first
-          user.latitude   = coordinates[0] rescue nil
-          user.longitude  = coordinates[1] rescue nil
+        prefill_profile(user)
 
-          if locationResult.city.blank?
-            user.zip      = locationResult.state rescue nil
-          else
-            user.zip      = locationResult.city rescue locationResult.state
-          end
-          user.country    = locationResult.country_code rescue nil
-        end
-
-        user.services.build(:provider => "facebook", :uid => data.id.to_s, :uname => data.username, :uemail => data.email)
+        user.services.build(:provider => "facebook", :uid => @data.id.to_s, :uname => @data.username, :uemail => @data.email)
         user.save!
 
         if user.persisted?
@@ -54,7 +36,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
           sign_in_and_redirect(:user, user)
         else
-          session["devise.facebook_data"] = omniauth
+          session["devise.facebook_data"] = @omniauth
           redirect_to new_user_registration_url
         end
       end
@@ -62,24 +44,52 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def soundcloud
-    omniauth = request.env['omniauth.auth']
-    data = omniauth.extra.raw_info
-    auth = Service.find_by_provider_and_uid("soundcloud", data.id.to_s)
+    provider = 'soundcloud'
+    auth = find_provider('soundcloud')
+
     if auth
-      flash[:notice] = t(:'services.signed_in_successful_via') + " soundcloud"
-      sign_in_and_redirect(:user, auth.user)
-      session["origin"] = "Soundcloud"
+      sign_in_with_provider(auth, provider)
     else
       if user_signed_in?
-        current_user.update_column('soundcloud_username', data.permalink)
-        current_user.services.create(:provider => "soundcloud", :uid => data.id.to_s, :uname => data.permalink, :uemail => "")
+        current_user.update_column('soundcloud_username', @data.permalink)
+        current_user.services.create(:provider => "soundcloud", :uid => @data.id.to_s, :uname => @data.permalink, :uemail => "")
         flash[:notice] = t(:'services.logged_in_via') + " Soundcloud " + t(:'services.added_to_account')
         redirect_to edit_user_path(current_user)
       else
-        flash[:error] =  "Soundcloud" + " " + t(:'services.account_creation_error', :service_route => "Soundcloud" )
+        flash[:error] =  provider.capitalize + " " + t(:'services.account_creation_error', :service_route => "Soundcloud" )
         redirect_to new_user_session_path
       end
     end
+  end
+
+  def sign_in_with_provider(auth, provider)
+    flash[:notice]    = t(:'services.signed_in_successful_via') + " " + provider
+    session["origin"] = provider.capitalize
+    sign_in_and_redirect(:user, auth.user)
+  end
+
+  def prefill_profile(user)
+    if @omniauth.extra.raw_info.location.present?
+      locationName    = @data.location.name
+      coordinates     = Geocoder.coordinates(locationName)
+      locationResult  = Geocoder.search(locationName).first
+      user.latitude   = coordinates[0] rescue nil
+      user.longitude  = coordinates[1] rescue nil
+
+      user.zip = if locationResult.city.blank?
+        locationResult.state rescue nil
+      else
+        locationResult.city rescue locationResult.state
+      end
+      user.country    = locationResult.country_code rescue nil
+    end
+  end
+
+  def create_facebook_auth(user)
+    user.services.create(:provider => "facebook", :uid => @data.id.to_s, :uname => @data.username, :uemail =>  @data.email)
+    flash[:notice] = t(:'services.logged_in_via') + " Facebook " + t(:'services.added_to_account') + " " + @data.email + " " + t(:'services.signed_in_successful')
+    session[:origin] = "Facebook"
+    sign_in_and_redirect(:user, user)
   end
 
   def passthru
@@ -87,5 +97,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # Or alternatively,
     raise ActionController::RoutingError.new('Not Found')
   end
+
+  def find_provider(provider)
+    Service.find_by_provider_and_uid(provider, @data.id.to_s)
+  end
+
+  private
+
+    def request_env_data
+      @omniauth = request.env['omniauth.auth']
+      @data     = @omniauth.extra.raw_info
+    end
+
 
 end
