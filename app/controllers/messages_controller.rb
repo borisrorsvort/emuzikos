60b05d1dcdea8105f7abf9570f1813f5c7b897ce
Boris Rorsvort <boris.rorsvort@trustedfamily.net>
2013-06-14 12:26:38 +0200

    refactored message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,14 +4,10 @@ class MessagesController < ApplicationController
   def index
     if params[:mailbox] == "sent"
       @messages = @current_user.sent_messages.order("created_at").page(params[:page]).per(AppConfig.site.results_per_page)
-      mixpanel.track 'Message box view', {
-        :box => "Sentbox"
-      }
+      mixpanel.track 'Message box view', { :box => "Sentbox" }
     else
       @messages = @current_user.received_messages.order("created_at").page(params[:page]).per(AppConfig.site.results_per_page)
-      mixpanel.track 'Message box view', {
-        :box => "Inbox"
-      }
+      mixpanel.track 'Message box view', { :box => "Inbox" }
     end
     if request.xhr?
       render @messages
@@ -19,48 +15,33 @@ class MessagesController < ApplicationController
   end
 
   def show
+    @message                = Message.read_message(params[:id], current_user)
+    @reply_to               = @message.sender.id
+    @new_message            = Message.new
+    @new_message.body       = "\n\n*Original message*\n\n #{@message.body}"
+    @new_message.sender     = @current_user
+    @new_message.recipient  = @message.sender
+
     mixpanel.track 'Viewed message'
-    @message = Message.read_message(params[:id], current_user)
-
-    @reply_to = @message.sender.id
-
-    @new_message = Message.new
-    @new_message.body = "\n\n*Original message*\n\n #{@message.body}"
-    @new_message.sender = @current_user
-    @new_message.recipient = @message.sender
   end
 
-  # def new
-  #   @message = Message.new
-  #   @old_message = Message.find(params[:old_message])
-  #   @message.sender = @current_user
-  #   @message.recipient = User.find(params[:reply_to])
-
-  #   if params[:reply_to]
-  #     @message.subject = "Re: #{@old_message.subject}"
-  #     @message.body = "\n\n*Original message*\n\n #{@old_message.body}"
-  #   end
-  # end
-
   def create
-    @message = Message.new(message_params)
-    @message.sender = @current_user
-    @message.recipient = User.find(params[:message][:to])
+    @message            = Message.new(message_params)
+    @message.sender     = @current_user
+    @message.recipient  = User.find(params[:message][:to])
 
     respond_to do |format|
       if @message.save
+        notify_user(@message, @current_user)
         mixpanel.track 'Submited message form'
 
         format.html {
-          # redirect_to :back, success: t('gflash.messages.create.success')
           redirect_to users_path, success: t('gflash.messages.create.success')
           gflash :success => true
         }
         format.js {
           flash.now[:success] = t('gflash.messages.create.success')
         }
-
-        Notifier.user_message(@message, @current_user, @message.recipient).deliver if @message.recipient.prefers_message_notifications == true
       else
         format.html {
           redirect_to :back
@@ -94,6 +75,10 @@ class MessagesController < ApplicationController
   end
 
   private
+
+    def notify_user(message, current_user)
+      Notifier.user_message(message, current_user, message.recipient).deliver if message.recipient.prefers_message_notifications == true
+    end
 
     def message_params
       params.require(:message).permit(:to, :body, :reply_to)
